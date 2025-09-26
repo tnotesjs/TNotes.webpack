f830b581c1b6b3bf73a56b52b568bd97efff4e5d
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-09-26 19:28:38 +0800

    update: 2 files modified

--- .vitepress/components/Layout/Swiper.vue
+++ .vitepress/components/Layout/Swiper.vue
@@ -24,7 +24,7 @@ const initSwiper = () => {
         if (!container || !tabsEl) return
 
         const instance = new Swiper(container, {
-          loop: false,
+          // loop: false,
           // TODO 可配置
           // effect: 'fade',
           speed: 0, // 禁用动画
@@ -44,7 +44,42 @@ const initSwiper = () => {
 
         // 生成 tabs（文案来自 data-title；为空则为 'img'）
         const slides = wrap.querySelectorAll('.swiper-slide')
+        const isTnTabNavVisible = slides.length >= 2
         tabsEl.innerHTML = ''
+
+        // 微调 tabs 容器样式
+        if (isTnTabNavVisible) {
+          tabsEl.style.padding = '0 0.8rem 0 3rem'
+        } else {
+          tabsEl.style.padding = '0 0.8rem'
+        }
+
+        // 左按钮 - 上一页
+        if (isTnTabNavVisible) {
+          const prevBtn = document.createElement('button')
+          prevBtn.type = 'button'
+          prevBtn.className = 'tn-tab-nav tn-tab-prev'
+          prevBtn.textContent = '<'
+          prevBtn.title = '上一页'
+          prevBtn.addEventListener('click', () => {
+            if (instance.activeIndex === 0) {
+              // 到头 -> 跳到最后一张
+              instance.slideTo(slides.length - 1)
+            } else {
+              instance.slidePrev()
+            }
+            setTimeout(() => {
+              updateContainerHeight(container, instance.activeIndex)
+            }, 0)
+          })
+          tabsEl.appendChild(prevBtn)
+
+          const line = document.createElement('span')
+          line.className = 'tn-tab-nav tab-tab-line'
+          line.textContent = '/'
+          tabsEl.appendChild(line)
+        }
+
         slides.forEach((slide, i) => {
           const label = slide.getAttribute('data-title') || 'img'
           const btn = document.createElement('button')
@@ -60,6 +95,27 @@ const initSwiper = () => {
           })
           tabsEl.appendChild(btn)
         })
+
+        // 右按钮 - 下一页
+        if (isTnTabNavVisible) {
+          const nextBtn = document.createElement('button')
+          nextBtn.type = 'button'
+          nextBtn.className = 'tn-tab-nav tn-tab-next'
+          nextBtn.textContent = '>'
+          nextBtn.title = '下一页'
+          nextBtn.addEventListener('click', () => {
+            if (instance.activeIndex === slides.length - 1) {
+              // 到头 -> 跳到第一张
+              instance.slideTo(0)
+            } else {
+              instance.slideNext()
+            }
+            setTimeout(() => {
+              updateContainerHeight(container, instance.activeIndex)
+            }, 0)
+          })
+          tabsEl.appendChild(nextBtn)
+        }
 
         swiperInstances.value.push(instance)
       })
@@ -115,14 +171,21 @@ watch(
 <style>
 /* 顶部 tabs（随页面滚动吸顶，兼容 VitePress 顶栏） */
 .tn-swiper-tabs {
-  position: sticky;
-  top: var(--vp-nav-height);
+  position: relative;
+  /* position: sticky;
+  top: var(--vp-nav-height); */
   z-index: 20;
   display: flex;
   overflow-x: auto;
   overflow-y: hidden;
   gap: 0.5rem;
+  /* 
+  通过 JS 控制 padding
+  tab 数大于等于 2，预留出左右两侧的 padding 给 tab-nav 按钮
+  padding: 0 0.8rem 0 3rem;
+  tab 数小于 2，减少 padding
   padding: 0 0.8rem;
+  */
   background: var(--vp-c-bg);
   border-bottom: 1px solid var(--vp-c-divider);
   border-radius: 8px 8px 0 0;
@@ -161,6 +224,39 @@ watch(
   background-color: var(--vp-code-tab-active-bar-color);
 }
 
+/* tab 左右导航按钮 */
+.tn-tab-nav {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background: transparent;
+  border: none;
+  font-size: 0.8rem;
+  cursor: pointer;
+  color: var(--vp-code-tab-text-color);
+  padding: 0 0.3rem;
+  transition: all 0.25s;
+  /* 无法被选中 */
+  user-select: none; /* 标准 */
+  -webkit-user-select: none; /* 兼容 Safari/旧版 Chrome */
+  -ms-user-select: none; /* 兼容旧版 IE/Edge */
+  -webkit-touch-callout: none; /* 禁止 iOS Safari 长按时弹出菜单 */
+}
+.tn-tab-prev {
+  left: 0.5rem;
+}
+.tab-tab-line {
+  left: 1.1rem;
+  color: var(--vp-code-tab-active-bar-color);
+}
+.tn-tab-next {
+  left: 1.5rem;
+}
+.tn-tab-prev:hover,
+.tn-tab-next:hover {
+  color: var(--vp-code-tab-active-bar-color);
+  font-size: 1rem;
+}
 /* 修改 swiper-container 样式以支持动态高度 */
 .swiper-container {
   width: 100%;
