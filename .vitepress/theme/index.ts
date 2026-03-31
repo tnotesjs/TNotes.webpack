abdcb1b1bf87e46a2441d44282425bfe28454611
Tdahuyou <dahuyoutop@gmail.com>
2026-03-31 21:52:11 +0800

    chore: migrate from submodule to @tnotesjs/core npm package

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,11 +3,10 @@
  *
  * 主题入口文件模块 - 连接层
  *
- * 所有主题逻辑封装在 TNotes.core 中，此文件仅作为 VitePress 入口。
+ * 所有主题逻辑封装在 @tnotesjs/core 中，此文件仅作为 VitePress 入口。
  *
  * !注意：主题入口文件模块的位置是 VitePress 规定的，不能更改位置！
  */
-import { defineNotesTheme } from '../tnotes/vitepress/theme'
+import { defineNotesTheme } from '@tnotesjs/core/vitepress/theme'
 
 export default defineNotesTheme()
-
